449f239e9d70e1220d26bc270826cb2c8fb666a6
MEASDEV <sonfacknelsonmandela@gmail.com>
2025-03-23 16:08:50 +0100

    Refactor chat layout to include ChatSidebar and user authentication

--- src/app/(chat)/layout.tsx
+++ src/app/(chat)/layout.tsx
@@ -1,8 +1,19 @@
+import { ChatSidebar } from "./components";
+import { serverClient } from "@/lib";
+
 export default async function ChatLayout({ children }: { children: React.ReactNode }) {
+  const supabase = await serverClient();
+  const user = await supabase.auth.getUser();
+
   return (
     <div className="h-screen flex flex-col bg-slate-100 overflow-hidden xl:p-5">
       <div className="max-w-6xl mx-auto w-full h-full">
-        {children}
+        <div className="flex gap-5 h-full">
+          <div className="hidden h-full md:block md:w-[35%] xl:w-[30%]">
+            <ChatSidebar user={user} />
+          </div>
+          {children}
+        </div>
       </div>
     </div>
   );
--- src/app/(chat)/page.tsx
+++ src/app/(chat)/page.tsx
@@ -1,18 +1,8 @@
 import ChatDetail from "./components/chat-detail";
-import { ChatSidebar } from "./components";
-import { serverClient } from "@/lib";
 
-export default async function Home() {
-  const supabase = await serverClient();
-
-  const user = await supabase.auth.getUser();
+export default async function ChatDetailPage() {
 
   return (
-    <div className="flex gap-5 h-full">
-      <div className="hidden h-full md:block md:w-[35%] xl:w-[30%]">
-        <ChatSidebar user={user} />
-      </div>
-      <ChatDetail />
-    </div>
+    <ChatDetail />
   );
 }
